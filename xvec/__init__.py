61539a5df55b82c33de4b4017557852d7f24e436
Benoit Bovy <benbovy@gmail.com>
2022-11-18 09:05:42 +0100

    fix __version__

--- xvec/__init__.py
+++ xvec/__init__.py
@@ -4,7 +4,7 @@ from .index import GeoVectorIndex  # noqa
 from .strtree import ShapelySTRTreeIndex  # noqa
 
 try:
-    __version__ = version("package-name")
+    __version__ = version("xvec")
 except PackageNotFoundError:  # noqa
     # package is not installed
     pass
