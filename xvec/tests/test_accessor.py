52cf9dc22496ab9742d0a1917f8275d9baf731cb
Martin Fleischmann <martin@martinfleischmann.net>
2023-02-01 15:03:35 +0100

    fix the other side

--- xvec/tests/test_accessor.py
+++ xvec/tests/test_accessor.py
@@ -401,11 +401,11 @@ def test_to_geopandas_dataset(traffic_dataset, geom_array):
         }
     ).set_geometry("destination", crs=26915)
     expected["origin"] = gpd.GeoSeries(expected["origin"], crs=26915)
-    expected["hour"] = expected["hour"].astype("int64")
 
     actual = traffic_dataset.sel(
         origin=shapely.Point(1, 2), hour=0, day="2023-01-01", mode="car"
     ).xvec.to_geopandas()
+    actual["hour"] = actual["hour"].astype("int64")  # win fix
 
     assert_geodataframe_equal(expected, actual)
 
