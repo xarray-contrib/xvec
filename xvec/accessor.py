00413266942282bed035cca82e92e184f8cecf7c
Martin Fleischmann <martin@martinfleischmann.net>
2025-05-03 14:25:01 +0200

    ensure we always get a GeoSeries

--- xvec/accessor.py
+++ xvec/accessor.py
@@ -963,12 +963,13 @@ class XvecAccessor:
         # ensure CRS of all columns is preserved
         for c in df.columns:
             if c in self._geom_coords_all:
-                # Even though xarray supports extension arrays as coords as of 2024.4.0, it is still possible
-                # that people will construct objects that have a geometry column with object dtype.
-                if df[c].dtype == "object":
-                    df[c] = gpd.GeoSeries(
-                        df[c], crs=self._obj[c].attrs.get("crs", None)
-                    )
+                # there is a geopandas bug that does not allow upcasting a series
+                # with a geometry dtype directly
+                df[c] = gpd.GeoSeries(
+                    df[c].values,
+                    crs=self._obj[c].attrs.get("crs", None),
+                    index=df[c].index,
+                )
 
         if geometry is not None:
             if geometry not in self._geom_coords_all:  # variable geometry
