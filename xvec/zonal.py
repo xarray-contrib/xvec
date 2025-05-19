0e900b62c114fb09126fb6ef950322c1bf7e2ca2
Jake Wilkins <jake@epoch.blue>
2025-05-19 15:59:45 +0100

    Addition of the strategy argument for _zonal_stats_exactextract, allows for the specification of feature-sequential or raster-sequential

--- xvec/zonal.py
+++ xvec/zonal.py
@@ -309,6 +309,7 @@ def _zonal_stats_exactextract(
     y_coords: Hashable,
     stats: str | Callable | Sequence[str | Callable | tuple] = "mean",
     name: str = "geometry",
+    strategy: str = "feature-sequential"
 ) -> xr.DataArray | xr.Dataset:
     """Extract the values from a dataset indexed by a set of geometries
 
@@ -363,7 +364,7 @@ def _zonal_stats_exactextract(
                 raise ValueError(f"{stat} is not a valid aggregation.")
 
         results, original_shape, coords_info, locs = _agg_exactextract(
-            acc, geometry, crs, x_coords, y_coords, stats, name, original_is_ds
+            acc, geometry, crs, x_coords, y_coords, stats, name, original_is_ds, strategy
         )
         i = 0
         for stat in stats:  # type: ignore
@@ -393,7 +394,7 @@ def _zonal_stats_exactextract(
         )
     elif isinstance(stats, str):
         results, original_shape, coords_info, _ = _agg_exactextract(
-            acc, geometry, crs, x_coords, y_coords, stats, name, original_is_ds
+            acc, geometry, crs, x_coords, y_coords, stats, name, original_is_ds, strategy
         )
         # Unstack the result
         arr = results.values.reshape(original_shape)
@@ -429,6 +430,7 @@ def _agg_exactextract(
     stats: str | Callable | Iterable[str | Callable | tuple] = "mean",
     name: str = "geometry",
     original_is_ds: bool = False,
+    strategy: str = "feature-sequential"
 ):
     """Extract the values from a dataset indexed by a set of geometries
 
@@ -492,7 +494,7 @@ def _agg_exactextract(
 
     # Aggregation result
     gdf = gpd.GeoDataFrame(geometry=geometry, crs=crs)
-    results = exactextract.exact_extract(rast=data, vec=gdf, ops=stats, output="pandas")
+    results = exactextract.exact_extract(rast=data, vec=gdf, ops=stats, output="pandas", strategy=strategy)
     # Get all the dimensions execpt x_coords, y_coords, they will be used to stack the
     # dataarray later
     if original_is_ds is True:
